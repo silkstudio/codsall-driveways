1e36935e88f42f488c093ecc2d2d32f75337cfc7
Nathan Hughes <Nathan@Nathans-MacBook-Pro.local>
2020-07-21 09:47:19 +0100

    update boilerplate.js, footer.scss and page transitions

--- js/app.js
+++ js/app.js
@@ -1,30 +1,82 @@
-// Init
+/*!
+ * Codsall Driveways V 0.1: Additional javascript for increased site functionality
+ * (c) 2020 Silk Studio
+ */
 
-const editorStyles = document.getElementById('editor-styles');
+/**
+ * Written by Nathan Hughes on 18/07/20.
+ *
+ * Silk Studio - development + design
+ * https://silkstudio.co
+ * https://github.com/silkstudio
+ *
+ */
 
-let intViewportWidth = window.innerWidth;
-let intViewportHeight = window.innerHeight;
-const mobileNav = document.querySelector('.mobile-nav');
+// ==========================================================================
+// App.js
+// ==========================================================================
 
-let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+// Page transitions
+const transitionElement = document.querySelector('.main');
 
-function delay(URL) {
+const delay = (URL) => {
+	transitionElement.classList.remove('main__in-view');
 	setTimeout(function() {
 		window.location = URL;
-	}, 1000);
-}
+	}, 250);
+};
 
-function init() {
-	editorStyles.parentNode.removeChild(editorStyles);
-	mobileNav.style.height = `${intViewportHeight}px`;
-	mobileNav.style.width = `${intViewportWidth}px`;
-	quicklink.listen();
+document.addEventListener('click', function(event) {
+	if (event.target.tagName === 'A') {
+		event.preventDefault();
+		delay(event.target.href);
+	} else if (event.target.parentNode.tagName === 'A') {
+		event.preventDefault();
+		delay(event.target.parentNode.href);
+	}
+});
+
+/*
+
+
+
+
+
+
+
+
+*/
+// Mobile menu sizing
+let intViewportWidth = window.innerWidth;
+let intViewportHeight = window.innerHeight;
+// const mobileNav = document.querySelector('.mobile-nav');
+
+const editorStyles = document.getElementById('editor-styles');
+let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+/*
+
+
+
+
+
+
+
+
+*/
+// Initialise page
+init = () => {
+	//editorStyles.parentNode.removeChild(editorStyles);
+	//mobileNav.style.height = `${intViewportHeight}px`;
+	//mobileNav.style.width = `${intViewportWidth}px`;
+	//quicklink.listen();
 	if (!isMobile && intViewportWidth >= 992) {
 		luxy.init({
 			wrapper      : '#luxy',
 			wrapperSpeed : 0.065
 		});
 	}
-}
+	transitionElement.classList.add('main__in-view');
+};
 
 window.onload = init();
