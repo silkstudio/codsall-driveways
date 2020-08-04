cea402c863689b3ff93f97391d01ee3d94eae6bc
Nathan Hughes <Nathan@Nathans-MacBook-Pro.local>
2020-08-04 10:38:54 +0100

    updates  4/8

--- js/pages/home.js
+++ js/pages/home.js
@@ -32,6 +32,7 @@ let stepCount;
 let updatedNextVal;
 let updatedPrevVal;
 let fcScrollMax;
+let scrollInstance;
 
 //
 // Init variables
@@ -43,12 +44,12 @@ const clickDragInit = () => {
 	updatedNextVal = cw;
 	updatedPrevVal = 0;
 	fcScrollMax = cw * (fcChildren.length - 2);
-	// console.log(`variables initiated, as follows:
-	// fcChildren: 	${fcChildren}
-	// cw: 			${cw}
-	// updatedNextVal: ${updatedNextVal}
-	// fcScrollMax: 	${fcScrollMax}
-	// `);
+	console.log(`variables initiated, as follows:
+	fcChildren: 	${fcChildren}
+	cw: 			${cw}
+	updatedNextVal: ${updatedNextVal}
+	fcScrollMax: 	${fcScrollMax}
+	`);
 };
 
 // Recalculate metrics if the window is resized
@@ -59,6 +60,19 @@ window.onresize = setTimeout(recalcMetrics, 1000);
 
 //
 // Define new scrollbooster instance
+
+// if (!isMobile || window.innerWidth >= 992) {
+// 	scrollInstance = new ScrollBooster({
+// 		viewport   : fcScrollContainer,
+// 		content    : fcScrollContent,
+// 		direction  : 'horizontal',
+// 		scrollMode : 'transform',
+// 		friction   : 0.1
+// 	});
+// } else {
+// 	scrollInstance = null;
+// }
+
 const fcScroll = new ScrollBooster({
 	viewport   : fcScrollContainer,
 	content    : fcScrollContent,
@@ -94,56 +108,63 @@ const setTranslateValue = () => {
 //
 // Right arrow click
 const fcScrollRight = () => {
-	// On click, scroll to the updatedNextVal
-	fcScroll.scrollTo({ x: updatedNextVal, y: 0 });
-	// Increase the exsisting value by clientWidth
-	updatedNextVal += cw;
-	// If the user has reached the end, stop them from scrolling further
-	if (updatedNextVal > fcScrollMax) {
-		updatedNextVal = fcScrollMax;
-		updatedPrevVal = updatedNextVal - cw;
-	} else {
-		updatedPrevVal = updatedNextVal - cw * 2;
+	if (!isMobile || window.innerWidth >= 992) {
+		// On click, scroll to the updatedNextVal
+		fcScroll.scrollTo({ x: updatedNextVal, y: 0 });
+		// Increase the exsisting value by clientWidth
+		updatedNextVal += cw;
+		// If the user has reached the end, stop them from scrolling further
+		if (updatedNextVal > fcScrollMax) {
+			updatedNextVal = fcScrollMax;
+			updatedPrevVal = updatedNextVal - cw;
+		} else {
+			updatedPrevVal = updatedNextVal - cw * 2;
+		}
 	}
 };
 
 //
 // Left arrow click
 const fcScrollLeft = () => {
-	// On click, scroll to the updatedPrevVal
-	fcScroll.scrollTo({ x: updatedPrevVal, y: 0 });
-	// Check whether user has come from the end and if not decrease both values by clientWidth
-	if (updatedNextVal === fcScrollMax && updatedPrevVal === fcScrollMax - cw) {
-		updatedNextVal = fcScrollMax;
-		updatedPrevVal = fcScrollMax - cw * 2;
-	} else if (updatedPrevVal <= 0 || updatedNextVal <= cw) {
-		updatedPrevVal = 0;
-		updatedNextVal = cw;
-	} else {
-		updatedPrevVal -= cw;
-		updatedNextVal -= cw;
+	if (!isMobile || window.innerWidth >= 992) {
+		// On click, scroll to the updatedPrevVal
+		fcScroll.scrollTo({ x: updatedPrevVal, y: 0 });
+		// Check whether user has come from the end and if not decrease both values by clientWidth
+		if (updatedNextVal === fcScrollMax && updatedPrevVal === fcScrollMax - cw) {
+			updatedNextVal = fcScrollMax;
+			updatedPrevVal = fcScrollMax - cw * 2;
+		} else if (updatedPrevVal <= 0 || updatedNextVal <= cw) {
+			updatedPrevVal = 0;
+			updatedNextVal = cw;
+		} else {
+			updatedPrevVal -= cw;
+			updatedNextVal -= cw;
+		}
 	}
 };
 
 //
 // Mouse interactions
-fcScrollArrwRight.addEventListener('click', fcScrollRight);
-fcScrollArrwLeft.addEventListener('click', fcScrollLeft);
 
-fcScrollContent.onmouseover = () => {
-	fcScrollContent.style.cursor = 'grab';
-};
+if (!isMobile || window.innerWidth >= 992) {
+	fcScrollArrwRight.addEventListener('click', fcScrollRight);
+	fcScrollArrwLeft.addEventListener('click', fcScrollLeft);
 
-fcScrollContent.onmousedown = () => {
-	fcScrollContent.style.cursor = 'grabbing';
-	//Fallback in case mouse leaves the viewport before releasing click
-	setTimeout(setTranslateValue, 900);
-};
+	fcScrollContent.onmouseover = () => {
+		fcScrollContent.style.cursor = 'grab';
+	};
 
-fcScrollContent.onmouseup = () => {
-	fcScrollContent.style.cursor = 'grab';
-	setTimeout(setTranslateValue, 1000);
-};
+	fcScrollContent.onmousedown = () => {
+		fcScrollContent.style.cursor = 'grabbing';
+		//Fallback in case mouse leaves the viewport before releasing click
+		setTimeout(setTranslateValue, 900);
+	};
+
+	fcScrollContent.onmouseup = () => {
+		fcScrollContent.style.cursor = 'grab';
+		setTimeout(setTranslateValue, 1000);
+	};
+}
 
 window.onload = setTimeout(clickDragInit, 500);
 
